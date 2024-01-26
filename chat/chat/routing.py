e65761beb97013353579e8ae09716769991a80b8
Jack Chamberlain <jack.chamberlain11@yahoo.com>
2024-01-26 15:27:49 -0600

    WIP

---
+++ chat/chat/routing.py
@@ -0,0 +1,13 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+#import app.routing
+from django.urls import re_path
+from app.consumers import TextRoomConsumer
+websocket_urlpatterns = [
+    re_path(r'^ws/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
+]
+application = ProtocolTypeRouter({
+    "websocket":
+    URLRouter(
+        websocket_urlpatterns
+    )
+})
